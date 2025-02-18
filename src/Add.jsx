bd52426c5cb44100d6628f5ca453b4e7ec762785
JaswanthReddy5 <kanumuru.jaswanthreddy@gmail.com>
2025-02-18 23:51:32 +0530

    update
    added rooter and  animation in navbar

---
+++ src/Add.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Add =()=>{
+    return (
+        <div>
+            <button className=' bg-green-500 w-40 h-40 '>ADD</button>
+        </div>
+    );
+}
+
+
+export default Add;  //exporting the component
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './Navbar.css'
-import Navbar from './Navbar.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Add from "./Add";
 
 const Main = () => {
   return (
-    <div  style={{ height: "100vh", background: "#fff" }}>
+    <div style={{ height: "100vh", background: "#fff" }}>
       <Navbar />
+      <Outlet /> {/* Allows child routes to render here */}
     </div>
   );
 };
 
+const routing = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "Addbutton",
+        element: <Add />,
+      },
+    ],
+  },
+]);
+
 const root = createRoot(document.getElementById("root"));
-root.render(<Main />);
+root.render(<RouterProvider router={routing} />);
