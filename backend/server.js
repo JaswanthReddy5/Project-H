612a5f80f37f7150b39430529a570831ead081a3
JaswanthReddy5 <kanumuru.jaswanthreddy@gmail.com>
2025-09-09 01:06:40 +0530

    chore: remove temporary debug scripts

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,8 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
   origin: [
     'https://magnificent-kringle-05c986.netlify.app',
-    'https://project-h-zv5o.onrender.com'
+    'https://project-h-zv5o.onrender.com',
+    'http://localhost:5173',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
